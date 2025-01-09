fcfdddc41c2d4d39a15b9c7394b685ffa79affe1
João VIctor <joawrt8520@gmail.com>
2025-01-09 13:13:50 -0300

    feat: create enterQueue and exitQueue functions

--- activities/trabalho pilha/main.cpp
+++ activities/trabalho pilha/main.cpp
@@ -2,36 +2,50 @@
 #include<stack>
 using namespace std;
 
+void enterQueue(stack<char> *s, char value) {
+    s->push(value);
+}
+
+char exitQueue(stack<char> *s) {
+
+    if (s->empty()) {
+        abort();
+    }
+
+    stack<char> aux;
+
+    // move values from s1 to aux
+    while (!s->empty()) {
+        aux.push(s->top());
+        s->pop();
+    }
+
+    // get first of the queue
+    char response = aux.top();
+    aux.pop();
+
+    // move from aux to s1
+    while (!aux.empty()) {
+        s->push(aux.top());
+        aux.pop();
+    }
+
+    return response;
+
+}
+
 int main() {
 
-    int max;
-
-    // get quantity elements
-    cout << "> quantidade de elementos: ";
-    cin >> max;
-
-    // creating stacks
     stack<char> s1;
-    stack<char> s2;
 
-    // entry values
-    for (int i = 0; i < max; i++) {
-        cout << (i+1) << " letra: ";
-        char current;
-        cin >> current;
-        s1.push(current);
-    }
+    enterQueue(&s1, 'e');
+    enterQueue(&s1, 'n');
+    enterQueue(&s1, 'z');
+    enterQueue(&s1, 'o');
 
-    // move data from s1 to s2
-    for (int i = 0; i < max; i++) {
-        s2.push(s1.top());
-        s1.pop();
-    }
-
-    // unstack values
-    for (int i = 0; i < max; i++) {
-        cout << "\n> desempilha: " << s2.top();
-        s2.pop();
-    }
+    cout << exitQueue(&s1) << endl;
+    cout << exitQueue(&s1) << endl;
+    cout << exitQueue(&s1) << endl;
+    cout << exitQueue(&s1) << endl;
 
 }
